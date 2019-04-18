1fa46b6278fdec22353e4ae3efcc6d2f33166969
Hrishikesh Suresh <blackleghubot@github.com>
2019-04-18 19:25:11 +0530

    tried mser

--- image_cnn_processing.py
+++ image_cnn_processing.py
@@ -212,11 +212,34 @@ print(binary_car_image)
 
 filtered_data, labels = homomorphic_filter(csv_files)
 
-cv2.imshow('Homomorphic filtered output', filtered_data[100])
+cv2.imshow('Homomorphic filtered output', filtered_data[0])
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # character segmentation
+img = cv2.imread('data/crop_h1/I00000.png')
+mser = cv2.MSER_create()
+
+#Resize the image so that MSER can work better
+img = cv2.resize(img, (img.shape[1]*2, img.shape[0]*2))
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+vis = img.copy()
+
+regions = mser.detectRegions(gray)
+hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions[0]]
+cv2.polylines(vis, hulls, 1, (0,255,0)) 
+
+cv2.namedWindow('img', 0)
+cv2.imshow('img', vis)
+while(cv2.waitKey()!=ord('q')):
+    continue
+cv2.destroyAllWindows()
+
+
+cv2.imshow('Homomorphic filtered output', hulls[0])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 ##labelled_plate = measure.label(license_plate)
 ##fig, ax1 = plt.subplots(1)
 ##ax1.imshow(license_plate, cmap = "gray")
